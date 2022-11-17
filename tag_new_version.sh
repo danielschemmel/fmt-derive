3149b2df771f556d6d9ebf1b4d044d9ded56f2dd
Daniel Schemmel <daniel@schemmel.net>
2022-11-17 21:30:00 +0000

    use msrv when `cargo test`ing for release

--- tag_new_version.sh
+++ tag_new_version.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 cargo clippy -- -D warnings
-cargo test
-cargo test --all-features
+cargo +1.56.0 test
+cargo +1.56.0 test --all-features
 git diff --exit-code  # check if unstaged changes exist
 git diff --cached --exit-code  # check if staged, uncommitted changes exist
 exec cargo workspaces version --all --allow-branch main --exact --no-individual-tags --force \*
