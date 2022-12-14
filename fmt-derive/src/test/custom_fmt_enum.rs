cd686e10c87545bb4429e3ec1f9c203fb6189646
Daniel Schemmel <daniel@schemmel.net>
2022-12-14 06:12:40 +0000

    Also test display

--- fmt-derive/src/test/custom_fmt_enum.rs
+++ fmt-derive/src/test/custom_fmt_enum.rs
@@ -1,47 +1,52 @@
 use pretty_assertions::assert_eq;
 
-use crate::Debug;
+use crate::{Debug, Display};
 
 pub struct Unprintable(u32);
 
-#[derive(Debug)]
+#[derive(Debug, Display)]
 pub enum Tuple {
-	#[debug("V({})", t0)]
+	#[fmt("V({})", t0)]
 	Variant(u32),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Display)]
 pub enum TupleUnprintable {
-	#[debug("V({})", t0.0)]
+	#[fmt("V({})", t0.0)]
 	Variant(Unprintable),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Display)]
 pub enum Struct {
-	#[debug("A({})", x)]
+	#[fmt("A({})", x)]
 	VariantA { x: u32 },
-	#[debug("B({})", x)]
+	#[fmt("B({})", x)]
 	VariantB { x: u64 },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Display)]
 pub enum StructUnprintable {
-	#[debug("V({})", x.0)]
+	#[fmt("V({})", x.0)]
 	Variant { x: Unprintable },
 }
 
 #[test]
 fn tuple_variant_test() {
 	assert_eq!(format!("{:?}", Tuple::Variant(0)), "V(0)");
+	assert_eq!(format!("{}", Tuple::Variant(0)), "V(0)");
 	assert_eq!(format!("{:?}", TupleUnprintable::Variant(Unprintable(0))), "V(0)");
+	assert_eq!(format!("{}", TupleUnprintable::Variant(Unprintable(0))), "V(0)");
 }
 
 #[test]
 fn struct_variant_test() {
 	assert_eq!(format!("{:?}", Struct::VariantA { x: 0 }), "A(0)");
+	assert_eq!(format!("{}", Struct::VariantA { x: 0 }), "A(0)");
 	assert_eq!(format!("{:?}", Struct::VariantB { x: 0 }), "B(0)");
+	assert_eq!(format!("{}", Struct::VariantB { x: 0 }), "B(0)");
 	assert_eq!(
 		format!("{:?}", StructUnprintable::Variant { x: Unprintable(0) }),
 		"V(0)"
 	);
+	assert_eq!(format!("{}", StructUnprintable::Variant { x: Unprintable(0) }), "V(0)");
 }
