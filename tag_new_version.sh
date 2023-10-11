6b9ae93c938ddb743ee59a3e89d21aa0057b6fa2
Daniel Schemmel <daniel@schemmel.net>
2023-10-11 09:30:00 +0100

    update tagging script with new msrv

--- tag_new_version.sh
+++ tag_new_version.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 cargo clippy -- -D warnings
-cargo +1.64.0 test
-cargo +1.64.0 test --all-features
+cargo +1.66.0 test
+cargo +1.66.0 test --all-features
 git diff --exit-code  # check if unstaged changes exist
 git diff --cached --exit-code  # check if staged, uncommitted changes exist
 exec cargo workspaces version --all --allow-branch main --exact --no-individual-tags --force \*
